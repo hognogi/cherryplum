5cc4a1deb49fb66a67a0eb77515ebdffa20a2d29
Beni <beni@hognogi.com>
2016-09-18 18:48:03 +0300

    default view fixes

--- client/interfaces/web/app/pages/Index.js
+++ client/interfaces/web/app/pages/Index.js
@@ -24,13 +24,30 @@ var Index = React.createClass({
 				
 
 					<h1>This is the Web homepage</h1>
-
+ 
 					This is rendered using React
 
 
 					<h3>Switch to the admin interface to see the view rendered with Plain DoT.js <a href="/?setinterface=admin">by clicking here</a></h3>
-					<br /><br /><br /> <br /><br />
+					<br /><br /><br /> 
+					<h4>You can find more details on the <a href="https://github.com/hognogi/cherryplum">github repository</a> or simply <a href="http://www.hognogi.com/">contact me</a> with any questions </h4>
+					<br /><br /><br /> 
 
+
+					<h3>To edit the React part of the website be sure to run the gulp task inside the root folder</h3>
+
+					<pre>
+						cherryplum_folder&gt;gulp
+					</pre>
+
+					you can find the react app folder at: 
+					<pre>
+						/client/interfaces/web/app
+					</pre>
+
+					
+
+					<br /><br />
 					<h5>Here is some data from the repo located at <br /> <br />server/repositories/database/repository.js:</h5>
 
 					<pre>
